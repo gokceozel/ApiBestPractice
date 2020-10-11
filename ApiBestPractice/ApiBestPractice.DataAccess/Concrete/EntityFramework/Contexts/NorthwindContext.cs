36c6d8adb305aa59163a0e3eb9368ca72d90a32f
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-10-11 15:35:58 +0300

    context is added

---
+++ ApiBestPractice/ApiBestPractice.DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,20 @@
+﻿using ApiBestPractice.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiBestPractice.DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+       
+    }
+}
