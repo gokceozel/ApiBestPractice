ff68baf60ab7fb41842dc28b91b8e02cfd61dd3f
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-10-11 15:13:11 +0300

    repo

---
+++ ApiBestPractice/ApiBestPractice.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using ApiBestPractice.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiBestPractice.Core.DataAccess.EntityFramework
+{
+    public class EfEntityRepositoryBase<T,Context> 
+        where T:class,IEntity,new()
+        where Context:DbContext ,new()
+   
+    {
+    }
+}
