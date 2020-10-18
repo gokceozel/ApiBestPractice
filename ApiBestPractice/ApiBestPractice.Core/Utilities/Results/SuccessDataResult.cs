06d9b78bbf179a8a3a9d0dd3be4e93277e75b2ee
Gökçe Özel <oze.gokce.ozel@gmail.com>
2020-10-18 15:35:27 +0300

    result message in utilities  is added.

---
+++ ApiBestPractice/ApiBestPractice.Core/Utilities/Results/SuccessDataResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiBestPractice.Core.Utilities.Results
+{
+    public class SuccessDataResult<T> :DataResult<T>
+    {
+        public SuccessDataResult(T data, bool success,string message):base(data,true,message)
+        {
+
+        }
+        public SuccessDataResult(T data ,bool success):base(data,true)
+        {
+
+        }
+    }
+}
